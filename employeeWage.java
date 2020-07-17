c414306a9884b431cf947af19c5f384388a86645
Kandimalla-Sanjana <kandimallasanjana@gmail.com>
2020-07-17 20:47:13 +0530

    ADDED:Full time employee wage

--- employeeWage.java
+++ employeeWage.java
@@ -5,17 +5,15 @@ public class employeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int isFullTime=1;
+	    int ratePerHour=20;
+	    int hours=0,salary=0;
 		int check=(int)Math.floor((Math.random()*10)%2);
 		if(check==isFullTime)
 		{
-		    System.out.println("Employee is present");
+		    hours=8;
 		}
-		else
-		{
-		    System.out.println("Employee is absent");
-		}
-
-
+	    salary=hours*ratePerHour;
+	    System.out.println("Salary is:"+salary);
 	}
 
 }
