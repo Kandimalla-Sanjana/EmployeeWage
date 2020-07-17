b34b241e05004d96f1f989347f524a41d640bbb3
Kandimalla-Sanjana <kandimallasanjana@gmail.com>
2020-07-17 20:15:36 +0530

    Welcome message

---
+++ employeeWage.java
@@ -0,0 +1,11 @@
+package employee;
+
+public class employeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to employee wage computation problem.");
+
+	}
+
+}
